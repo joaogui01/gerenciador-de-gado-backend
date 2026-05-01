01184bbdb27355fdfb512ccabea5021bdd1fbbe3
João Guilherme Oliveira Pereira <oliveiralaive15@gmail.com>
2026-05-01 01:23:01 -0300

    feat: finaliza implementação da autenticação JWT e correção de metadados

---
+++ src/main/java/com/projeto/Gerenciador/de/Gado/config/TokenService.java
@@ -0,0 +1,52 @@
+package com.projeto.Gerenciador.de.Gado.config;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.projeto.Gerenciador.de.Gado.entity.Usuario;
+
+@Service
+public class TokenService {
+	
+	@Value("${api.security.token.secret}")
+	private String secret;
+	
+	public String generateToken(Usuario usuario) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(secret);
+			String token = JWT.create()
+					.withIssuer("auth-api")
+					.withSubject(usuario.getLogin())
+					.withExpiresAt(genExpirationDate())
+					.sign(algorithm);
+			return token;
+		} catch (JWTCreationException exception) {
+			throw new RuntimeException("Erro enquanto gerava token", exception);
+		}
+	}
+	
+	public String validateToken(String token) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(secret);
+			return JWT.require(algorithm)
+					.withIssuer("auth-api")
+					.build()
+					.verify(token)
+					.getSubject();	
+		} catch (JWTVerificationException exception) {
+			return "";
+		}
+	}
+	
+	private Instant genExpirationDate() {
+		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+	}
+}
--- src/main/java/com/projeto/Gerenciador/de/Gado/controllers/AutenticacaoController.java
+++ src/main/java/com/projeto/Gerenciador/de/Gado/controllers/AutenticacaoController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.Gerenciador.de.Gado.config.TokenService;
 import com.projeto.Gerenciador.de.Gado.dto.DadosAutenticacao;
+import com.projeto.Gerenciador.de.Gado.dto.DadosTokenJWT;
 import com.projeto.Gerenciador.de.Gado.entity.Usuario;
 import com.projeto.Gerenciador.de.Gado.repository.UsuarioRepository;
 
@@ -25,13 +27,18 @@ public class AutenticacaoController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private TokenService tokenService;
 
 	@PostMapping("/login")
 	public ResponseEntity<?> efetuarLogin (@RequestBody @Valid DadosAutenticacao dados){
-	    var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-		var autenticacao = manager.authenticate(token);
-	
-	    return ResponseEntity.ok().build();
+	    var usernamePassword = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+		var autenticacao = manager.authenticate(usernamePassword);
+		
+		var token = tokenService.generateToken((Usuario) autenticacao.getPrincipal());
+		
+	    return ResponseEntity.ok(new DadosTokenJWT(token));
 	}
 	
 	@PostMapping("/register")
